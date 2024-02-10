73d82a2c2d4ad4ca8ba49e85cecf03ccb870a8fb
Aminul Momin <bbcredcap3@gmail.com>
2024-02-09 20:32:51 -0600

    Develope Jenkins CICD Pipeline

--- scripts/envsetup.sh
+++ scripts/envsetup.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-if [ -d "env" ] 
+if [ -d ".venv" ] 
 then
     echo "Python virtual environment exists." 
 else
-    python3 -m venv env
+    python3 -m venv .venv
 fi
 
-source env/bin/activate
+source .venv/bin/activate
 
 
-pip3 install -r requirements.txt
+pip3 install -r requirements.txt --no-cache-dir
 
 if [ -d "logs" ] 
 then
